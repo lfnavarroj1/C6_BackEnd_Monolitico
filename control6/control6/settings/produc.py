ba6be70e7d003d9fb836565907f221d3a4e54b5a
Luis Felipe Navarro Julio <lf.navarroj@uniandes.edu.co>
2023-11-04 09:43:46 -0500

    Actualizando nginx

---
+++ control6/control6/settings/produc.py
@@ -0,0 +1,32 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db_backend_c6",
+        "USER": "postgres",
+        "PASSWORD": "nativa1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT=BASE_DIR/'staticfiles/'
+STATICFILES_DIRS=[BASE_DIR/'static']
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR/'media/'
