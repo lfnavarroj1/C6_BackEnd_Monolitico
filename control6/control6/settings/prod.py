65e44d8f123a7354e783c39810a65ec15a119165
Luis Felipe Navarro Julio <lf.navarroj@uniandes.edu.co>
2023-11-07 09:25:59 -0500

    Habilitando el true en el despliegue

--- control6/control6/settings/prod.py
+++ control6/control6/settings/prod.py
@@ -1,9 +1,9 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["159.65.246.228"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Database
