38dce6193a16582a6cf803bd45f9be0bf5ae0e2e
Sebastian8489 <sebastianv1pp@gmail.com>
2024-02-27 15:54:39 -0500

    Refactor
    Refactor of libretto

---
+++ control6/applications/libretto_management/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import(
+    CargarLibreto,
+    ListarLibreto,
+    ObtenerLibreto,
+    ActualizarLibreto,
+    EliminarLibreto,
+)
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    # # Libretos
+    path('cargar-libreto/', CargarLibreto.as_view(), name="cargar-libreto" ),# Revisar el proceso para eliminar.
+    path('listar-libreto/<pk>', ListarLibreto.as_view(), name="listar-libreto" ),
+    path('detalle-libreto/<pk>', ObtenerLibreto.as_view(), name="'detalle-libreto" ),
+    path('actualizar-libreto/<pk>', ActualizarLibreto.as_view(), name="'actualizar-libreto" ),
+    path('eliminar-libreto/<pk>', EliminarLibreto.as_view(), name="eliminar-libreto" ),
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
