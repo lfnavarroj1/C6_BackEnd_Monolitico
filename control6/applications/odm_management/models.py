07e490f9bbbd6578ee7f2afcda3c4c5974081f90
Sebastian8489 <sebastianv1pp@gmail.com>
2024-02-27 16:37:05 -0500

    Refactor
    Refactor of odm

--- control6/applications/odm_management/models.py
+++ control6/applications/odm_management/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from budgets_management.models import Valorizacion
+from .manager import OdmManager
 
 # Create your models here.
+
+class Odm(models.Model):
+    odm = models.BigIntegerField(primary_key=True)
+    valorizacion = models.ForeignKey(Valorizacion, on_delete=models.PROTECT)
+    agp = models.BigIntegerField()
+    protocolo=models.BigIntegerField()
+    solicitud = models.BigIntegerField()
+
+    objects=OdmManager()
